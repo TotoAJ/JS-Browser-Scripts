a290fb4b37af13f7c01af936654bae475d09f15f
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2023-05-04 11:55:26 -0700

    Update password-generator.js

--- password-generator.js
+++ password-generator.js
@@ -2,7 +2,7 @@
   const groups = 3;
   const charsPerGroup = 6;
   const separator = '-';
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
   let password = "";
   for (let i = 0; i < groups; i++) {
     for (let j = 0; j < charsPerGroup; j++) {

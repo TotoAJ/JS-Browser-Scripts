2552046b2f3495629c3ed78844da91b8bc79649a
TotoAJ <67079777+TotoAJ@users.noreply.github.com>
2023-05-04 11:38:13 -0700

    Create password-generator.js

---
+++ password-generator.js
@@ -0,0 +1,17 @@
+(function(){
+  const groups = 3; // number of groups of characters in the password
+  const charsPerGroup = 6; // number of characters in each group
+  const separator = '-'; // separator character between groups
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+  let password = "";
+  for (let i = 0; i < groups; i++) {
+    for (let j = 0; j < charsPerGroup; j++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    if (i < groups - 1) {
+      password += separator;
+    }
+  }
+  navigator.clipboard.writeText(password);
+  alert("Your password has been generated and copied to your clipboard:\n\n" + password);
+})();
